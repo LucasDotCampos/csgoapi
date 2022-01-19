2d0eb8b91c05569cfda8383c83449adf632cbe3c
LucasDotCampos <lucasdotcampos@gmail.com>
2022-01-19 16:41:17 -0300

    feature/swagger doc added: working fine

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,12 +5,15 @@ import route from "./routes";
 
 import "../http/typeorm/connection";
 import "dotenv/config";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({}));
 app.use(route);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(process.env.PORT, () => {
     console.log(`Running at ${process.env.PORT} port`);
